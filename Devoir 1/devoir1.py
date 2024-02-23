fba4a3595642401a27f34d57235e1229dbee50e7
pierrot <6772696d@gmail.com>
2024-02-23 17:52:23 +0100

    Complete devoir1.py

--- Devoir 1/devoir1.py
+++ Devoir 1/devoir1.py
@@ -36,15 +36,7 @@ def make_T(m,n,t):
 
 	return T
 
-def backward_substitution(R, b):
-    # Substitution arrière
-    n = len(b)
-    x = np.zeros_like(b, dtype=float)
 
-    for i in range(n - 1, -1, -1):
-        x[i] = (b[i] - np.dot(R[i, i+1:], x[i+1:])) / R[i, i]
-
-    return x
 
 def b(t,T,i,p):
 	# Coefficient de B-spline d'ordre p, au paramètre t et de noeuds T
@@ -54,9 +46,11 @@ def b(t,T,i,p):
 		u  = 0.0 if T[i+p ]  == T[i]   else (t-T[i])/(T[i+p]- T[i]) * b(t,T,i,p-1)
 		u += 0.0 if T[i+p+1] == T[i+1] else (T[i+p+1]-t)/(T[i+p+1]-T[i+1]) * b(t,T,i+1,p-1)
 		return u
+
 @numba.jit(nopython=True, parallel=False)
 def qr(A):
 	# Décomposition QR de la matrice A
+	# Note : On utilise ici l'algorithme de Gram-Schmidt modifié tel que vu au CM2
 	m,n = A.shape
 	Q = A.copy()
 	R = np.zeros((n,n))
@@ -72,6 +66,17 @@ def qr(A):
 			Q[:,j] -= R[k,j] * Q[:,k]
 	return Q,R
 
+@numba.jit(nopython=True)
+def backward_substitution(R, b):
+    # Substitution arrière
+    n = len(b)
+    x = np.zeros(n)
+
+    for i in range(n - 1, -1, -1):
+        x[i] = (b[i] - np.dot(R[i, i+1:], x[i+1:])) / R[i, i]
+
+    return x
+
 def lstsq(A,B):
 	# Résolution du système des moindres carrés donné par : A^T A X = A^T B
 	# en utilisant une décomposition QR
@@ -81,18 +86,53 @@ def lstsq(A,B):
 	_,n = R.shape
 	X = np.zeros((n,p))
 	for i in range(p):
-		X[:,i] = backward_substitution(R,B[:,i])
+		X[:,i] = backward_substitution(R,V[:,i])
 	return X
 
 
-# Data
-m = 400
-n = 30
+# Étude de la complexité de la décomposition QR
 
-A = np.random.rand(m,n)
-Q,R = qr(A)
-fig,axs = plt.subplots(1,2)
-axs[0].imshow(Q.T @ Q, cmap='Blues')
-axs[1].imshow(np.abs(R), cmap='Blues')
+# Caching de qr et lstsq
+dummyA = np.random.rand(2,2)
+dummyB = np.random.rand(2,2)
+lstsq(dummyA,dummyB)
 
+
+def computeTime(m, n):
+	# Fonction de calcul des temps en fonction de m et n
+	deltaT = np.zeros(m.shape)
+	for i in range(m.shape[0]):
+		for j in range(m.shape[1]):
+			A = np.random.rand(i+1,j+1)
+			t1 = time.time()
+			C = qr(A)
+			t2 = time.time()
+			deltaT[i,j] = t2 - t1
+
+	return deltaT
+
+# Definition des m et n à évaluer
+m_values = np.arange(10, 2000, 40)
+n_values = np.arange(10, 2000, 40)
+
+# Meshgrid de m_values et n_values
+m, n = np.meshgrid(m_values, n_values)
+
+# Calcul du mesh des temps de calcul
+z = computeTime(m,n)
+
+# Plot 3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot la surface
+ax.plot_surface(m, n, z, cmap='viridis')
+
+# Label et titre
+ax.set_xlabel('m')
+ax.set_ylabel('n')
+ax.set_zlabel('Temps de la factorisation QR')
+ax.set_title("Graphe de la complexité d'une factorisation QR en fonction de m et n < 1000")
+
+# Show the plot
 plt.show()
