74da6dbf7150fd2a9f7e1b44dae9bddfa2eec921
Pierre V <6772696d@gmail.com>
2024-03-07 13:40:05 +0100

    Tests on the dimensions

--- CM2/condition.py
+++ CM2/condition.py
@@ -12,7 +12,7 @@ kappa = np.linalg.cond(A)
 # print(f'{np.linalg.cond(A) = }')
 
 p = 1000
-delta = np.zeros((p,2))
+delta = np.zeros((p,m))
 for k in range(p):
 	Ap = A + 1e-10 * np.random.randn(m,m)
 	xp = np.linalg.solve(Ap, b)
@@ -22,7 +22,7 @@ fig,ax = plt.subplots()
 ax.scatter(delta[:,0], delta[:,1])
 circle = plt.Circle((0.0,0.0), kappa, fill=False)
 ax.add_patch(circle)
-
+print(delta)
 print(f'{kappa = }')
 print(f'{np.max(np.linalg.norm(delta, axis=1)) = }')
 
