800eef8eb83127d057926c952b5cb0f1c3f8ab94
Pierre V <6772696d@gmail.com>
2024-03-10 15:24:02 +0100

    Correction to complexity graph

--- Devoir 2/decompositionLU.py
+++ Devoir 2/decompositionLU.py
@@ -37,10 +37,10 @@ def para_lu_factorization(A):
 # Dummy example to compile code with numba :
 A = np.array([[4, 3], [6, 3]], dtype=float)
 x = np.array([1,2], dtype=float)
-t1 = time.time()
+t1 = time.perf_counter()
 L, U = lu_factorization(A)
 L, U = para_lu_factorization(A)
-t2 = time.time()
+t2 = time.perf_counter()
 CompileTime = t2 - t1
 
 
@@ -49,23 +49,23 @@ times = np.zeros((ns.size))
 paraTimes = np.zeros((ns.size))
 for i in range(ns.size):
     A = np.random.randn(ns[i],ns[i])
-    t1 = time.time()
+    t1 = time.perf_counter_ns()
     L, U = lu_factorization(A)
-    t2 = time.time()
+    t2 = time.perf_counter_ns()
     times[i] = t2-t1
-    t1 = time.time()
+    t1 = time.perf_counter_ns()
     L, U = para_lu_factorization(A)
-    t2 = time.time()
+    t2 = time.perf_counter_ns()
     paraTimes[i] = t2-t1
 
 
 plt.loglog(ns, times, label="Complexité single thread", color="blue")
 plt.loglog(ns, paraTimes, label="Complexité mutli-thread", color="yellow")
-plt.loglog(ns, 2/3 * ns**3 * 1e-9,color="orange", label="2/3 * n^3")
+plt.loglog(ns, 1/3 * ns**3,color="orange", label="1/3 * n^3")
 plt.title("Complexité temporelle de la décomposition LU")
 plt.xlabel("Echelle log de l'ordre n de A")
-plt.ylabel("Echelle log du temps de calcul de la décomposition LU")
+plt.ylabel("Echelle log du temps de calcul de la décomposition LU en nanosecondes")
 plt.legend()
 plt.show()
 
-print(f"Compile = {CompileTime}, Compute = {np.sum(times) + np.sum(paraTimes)}")
+print(f"Compile = {CompileTime}, Compute = {(np.sum(times) + np.sum(paraTimes))*1e-9}")
--- Devoir 2/unitTests.py
+++ Devoir 2/unitTests.py
@@ -1,3 +1,26 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-print(np.linspace(1,10, 10,dtype=int))
+A = np.random.randn(5,3)
+
+# L = np.tril(A)
+# U = np.triu(A)
+# 
+# ltl = L.T @ L
+# utu = U.T @ U
+# ll = L @ L
+# uu = U @ U
+# 
+# fig, axs = plt.subplots(2,2)
+# axs[0][0].imshow(ltl, cmap="Blues", label="ltl")
+# axs[0][0].set_title("L.T @ L")
+# axs[0][1].imshow(utu, cmap="Blues", label="utu")
+# axs[0][1].set_title("U.T @ U")
+# axs[1][0].imshow(ll, cmap="Blues", label="ll")
+# axs[1][0].set_title("L @ L")
+# axs[1][1].imshow(uu, cmap="Blues", label="uu")
+# axs[1][1].set_title("U @ U")
+
+
+plt.imshow(A.T @ A, cmap="Blues")
+plt.show()
