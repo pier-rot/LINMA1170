5d8ad0aa4f8db62440bda51877541ecd5c21d9f0
Pierre V <6772696d@gmail.com>
2024-03-11 16:38:30 +0100

    Complete Assignment with cached functions and Cholesky implementation and graph

--- Devoir 2/Rapport/decompositionLU.py
+++ Devoir 2/Rapport/decompositionLU.py
@@ -3,7 +3,7 @@ import numba
 import matplotlib.pyplot as plt
 import time
 
-@numba.jit(nopython=True, parallel=False)
+@numba.jit(nopython=True, parallel=False, cache=True)
 def lu_factorization(A):
     m, n = A.shape
 
@@ -18,7 +18,7 @@ def lu_factorization(A):
             U[i, k:] -= (factor * U[k, k:])
 
     return L, U
-@numba.jit(nopython=True, parallel=True)
+@numba.jit(nopython=True, parallel=True, cache=True)
 def para_lu_factorization(A):
     m, n = A.shape
 
@@ -35,20 +35,16 @@ def para_lu_factorization(A):
     return L, U
 
 
-@numba.jit(nopython=True, parallel=False)
+@numba.jit(nopython=True, parallel=True, cache=True)
 def cholesky(A):
     m,n = A.shape
     if (m != n):
         raise ValueError("A must be square")
     C = A
-    for k in range(n):
-        for j in range(k+1, n):
-            for i in range(j,n):
-                C[i,j] = C[i,j] - C[i,k]*C[j,k]/C[k,k]
-        
-        for i in range(k,n):
-            C[i,k] = C[i,k]/np.sqrt(C[k,k])
-
+    for k in numba.prange(m):
+        for j in numba.prange(k+1, m):
+            C[j,j:] -= C[k,j:]*C[k,j]/C[k,k]
+        C[k,k:] /= np.sqrt(C[k,k])
     return C
 
 # Dummy example to compile code with numba :
@@ -59,6 +55,7 @@ L, U = lu_factorization(A)
 L, U = para_lu_factorization(A)
 t2 = time.perf_counter()
 CompileTime = t2 - t1
+A = A.T @ A
 C = cholesky(A)
 
 # Computing data for graphs
@@ -66,30 +63,42 @@ ns = np.arange(100, 2000, 50)
 times = np.zeros((ns.size))
 paraTimes = np.zeros((ns.size))
 choTimes = np.zeros((ns.size))
+#for i in range(ns.size):
+#    A = np.random.randn(ns[i],ns[i])
+#    t1 = time.perf_counter_ns()
+#    L, U = lu_factorization(A)
+#    t2 = time.perf_counter_ns()
+#    times[i] = t2-t1
 for i in range(ns.size):
     A = np.random.randn(ns[i],ns[i])
-    t1 = time.perf_counter_ns()
-    L, U = lu_factorization(A)
-    t2 = time.perf_counter_ns()
-    times[i] = t2-t1
     t1 = time.perf_counter_ns()
     L, U = para_lu_factorization(A)
     t2 = time.perf_counter_ns()
     paraTimes[i] = t2-t1
+    
+    A = A.T @ A
     t1 = time.perf_counter_ns()
     C = cholesky(A)
     t2 = time.perf_counter_ns()
     choTimes[i] = t2-t1
 
+gain = np.multiply(paraTimes, 1/choTimes)
 
-plt.loglog(ns, times, label="Complexité single thread", color="blue")
-plt.loglog(ns, paraTimes, label="Complexité mutli-thread", color="yellow")
-plt.loglog(ns, 1/3 * ns**3,color="orange", label="1/3 * n^3")
-plt.loglog(ns, choTimes, label="Complexité Cholesky", color="green")
-plt.title("Complexité temporelle de la décomposition LU")
-plt.xlabel("Echelle log de l'ordre n de A")
-plt.ylabel("Echelle log du temps de calcul de la décomposition LU en nanosecondes")
-plt.legend()
+
+#plt.loglog(ns, times, label="Complexité single thread", color="blue")
+#plt.loglog(ns, paraTimes, label="Complexité mutli-thread", color="yellow")
+#plt.loglog(ns, 1/3 * ns**3,color="orange", label="1/3 * n^3")
+#plt.loglog(ns, choTimes, label="Complexité Cholesky", color="green")
+#plt.title("Complexité temporelle de la décomposition LU")
+#plt.xlabel("Echelle log de l'ordre n de A")
+#plt.ylabel("Echelle log du temps de calcul de la décomposition LU en nanosecondes")
+#plt.legend()
+#plt.show()
+
+plt.plot(ns, gain, label="Gain de temps de Cholesky par rapport à décomposition LU")
+plt.title("Complexité temporelle de l'algorithme de Cholesky")
+plt.xlabel("Taille n de matrice A n x n")
+plt.ylabel("Gain par rapport à la décomposition LU")
 plt.show()
 
-print(f"Compile = {CompileTime}, Compute = {(np.sum(times) + np.sum(paraTimes))*1e-9}")
+print(f"Compile = {CompileTime}, Compute = {(np.sum(times) + np.sum(paraTimes)+ np.sum(choTimes))*1e-9}")
--- Devoir 2/unitTests.py
+++ Devoir 2/unitTests.py
@@ -3,27 +3,5 @@ import matplotlib.pyplot as plt
 # import scipy as sp
 import numba
 
-@numba.jit(nopython=True, parallel=False)
-def cholesky_crout(A):
-    C = A
-    m,n = A.shape
-
-    for k in range(n):
-        for j in range(k+1, n):
-            for i in range(j,n):
-                C[i,j] = C[i,j] - C[i,k]*C[j,k]/C[k,k]
-        
-        for i in range(k,n):
-            C[i,k] = C[i,k]/np.sqrt(C[k,k])
-    return C
-
-n=10
-m= 15
-Cd = cholesky_crout(np.random.randn(2,2))
-x = np.random.randn(n)
-A = np.random.randn(m,n)
-A = A.T @ A
-L = cholesky_crout(A)
-lnp = np.linalg.cholesky(A)
-print(A @ x)
-print(lnp @ lnp.T @ x)
+A = np.random.randn(300,10)
+print((A.T @ A).shape)
