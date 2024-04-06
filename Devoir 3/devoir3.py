ff2633fd42f4cf6b0633ef0d2e9991a47a718d7b
Pierre V <6772696d@gmail.com>
2024-04-06 23:21:08 +0200

    Define dot product of 1D or 2D matrices

--- Devoir 3/devoir3.py
+++ Devoir 3/devoir3.py
@@ -1,15 +1,36 @@
 from numba import njit, prange
 import numpy as np
 
+## Constants
+c_type = np.complex64
+
 ## Helper functions
+# Global dot product
+@njit(cache=True)
+def dot(A : np.ndarray, B : np.ndarray) -> np.ndarray:
+    if (A.ndim == 1) and (B.ndim == 1) and (A.size == B.size):
+        return vdot(A,B)
+    
+    elif (A.ndim == 2 ) and (B.ndim == 2) and (A.shape[1] == B.shape[0]):
+        return MM_prod(A,B)
+    
+    elif (A.ndim == 1) and (B.ndim == 2) and (A.size == B.shape[0]):
+        return VM_prod(A,B)[0]
+    
+    elif (A.ndim == 2) and (B.ndim == 1) and (A.shape[1] == B.size):
+        return MV_prod(A,B)[:,0]
+    
+    else:
+        raise ValueError
+
+
 # Vector dot product
 @njit(parallel=True, cache=True)
-def dot(v1 : np.ndarray, v2 : np.ndarray):
+def vdot(v1 : np.ndarray, v2 : np.ndarray):
     if v1.shape[0] != v2.shape[0] :
         raise ValueError(f"Invalid shape : {v1.shape[0]} != {v2.shape[0]}")
-    
-    v1 = v1.astype(np.complex64)
-    v2 = v2.astype(np.complex64)
+    v1 = v1.astype(c_type)
+    v2 = v2.astype(c_type)
     
     sum = 0
     for i in prange(v1.shape[0]):
@@ -17,32 +38,46 @@ def dot(v1 : np.ndarray, v2 : np.ndarray):
     
     return sum
 
-# Matrix product   
+# Matrix * Matrix product   
 @njit(parallel=True, cache=True)
-def mprod(m1 : np.ndarray, m2 : np.ndarray) -> np.ndarray:
-    if m1.shape[1] != m2.shape[0]:
-        raise ValueError(f"m1 has {m1.shape[1]} columns while m2 has {m2.shape[0]} rows")
-
-    m1 = m1.astype(np.complex64)
-    m2 = m2.astype(np.complex64)
-
-    if m1.ndim == 1:
-        m1 = np.reshape((1,m1.size))
-    if m2.ndim == 1:
-        m2 = np.reshape((1,m2.size))
-
+def MM_prod(m1 : np.ndarray, m2 : np.ndarray) -> np.ndarray:
+    m1 = m1.astype(c_type)
+    m2 = m2.astype(c_type)
+    # product of m-by-n matrix m1 and n-by-p matrix m2 with m,n,p != 1
     m,n = m1.shape
     _,p = m2.shape
-
-    m3 = np.zeros((m,p),dtype=np.complex64)
-
+    
+    m3 = np.zeros((m,p), dtype=c_type)
     for i in prange(m):
         for j in prange(p):
-            for k in prange(n):
-                m3[i,j] += m1[i,k] * m2[k,j]
+            m3[i,j] = vdot(m1[i,:], m2[:,j])
 
+    #m1 = n1
     return m3
 
+
+# Vector * Matrix product
+@njit(cache=True)
+def VM_prod(m1 : np.ndarray, m2 : np.ndarray) -> np.ndarray:
+    m1 = m1.astype(c_type)
+    m2 = m2.astype(c_type)
+
+    a = np.zeros((1,m1.size), dtype=c_type)
+    a[0] = m1
+    return MM_prod(a,m2)
+    
+# Matrix * Vector product
+@njit(parallel=True, cache=True)
+def MV_prod(m1 : np.ndarray, m2 : np.ndarray) -> np.ndarray:
+    m1 = m1.astype(c_type)
+    m2 = m2.astype(c_type)
+
+    a = np.zeros((m2.size,1), dtype=c_type)
+    for i in prange(m2.size):
+        a[i] = np.array([m2[i]],dtype=c_type)
+    return MM_prod(m1,a)
+
+# Vector norm
 @njit(parallel=True, cache=True)
 def vnorm(v : np.ndarray):
     if v.ndim != 1:
@@ -68,7 +103,7 @@ def hessenberg(A : np.ndarray , P : np.ndarray):
         P (np.ndarray (with np.complex_ entries)): Un-initialised array which contains n x n unitary transformation matrix P
     """
 
-    
+
 
 # Transformation QR
 @njit(parallel=False, cache=True)
