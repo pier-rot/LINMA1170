7ee836284448e5497f5d5695ce3b6db9fde6ae32
Pierre V <6772696d@gmail.com>
2024-04-06 14:59:35 +0200

    Empty tests

--- Devoir 3/unitTests.py
+++ Devoir 3/unitTests.py
@@ -1,15 +1,3 @@
-from devoir3 import dot, mprod
+from devoir3 import dot, mprod, vnorm
 import numpy as np
 
-a = np.array([1,2,3])
-b = np.array([1,2,3])
-
-print(dot(a,b))
-
-A = np.array([[1,0]])
-A2 = np.array([1,0])
-B = np.array([[1,0],
-              [0,1]])
-print(mprod(A,B))
-print(A.shape)
-print(A2.reshape((1,A2.size)).shape)
